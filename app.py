4d62a934965c1ae325c414de442e3dcf9ccec0a7
Muhammad Moeenuddin <getmoeen@gmail.com>
2025-02-03 22:42:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,30 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # CloudAMQP connection
 AMQP_URL = "amqps://hfwdoxdg:1aeRZiQ9lXXeWwlFioDsGMCqNAfi_4sU@campbell.lmq.cloudamqp.com/hfwdoxdg" #"your_cloudamqp_url"
-params = pika.URLParameters(AMQP_URL)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
-channel.queue_declare(queue="image_resize")
+#params = pika.URLParameters(AMQP_URL)
+#connection = pika.BlockingConnection(params)
+#channel = connection.channel()
+#channel.queue_declare(queue="image_resize")
+
+def list_files(path):
+    # Check if the path exists
+    if os.path.exists(path):
+        # Iterate over the files and directories in the given path
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                print(os.path.join(root, file))
+    else:
+        print("The provided path does not exist.")
+
+def get_rabbitmq_connection():
+    try:
+        params = pika.URLParameters(AMQP_URL)
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        return connection, channel
+    except Exception as e:
+        print(f"Error connecting to RabbitMQ: {e}")
+        return None, None
 
 @app.route("/",methods=["GET"])
 def index():
@@ -42,14 +62,22 @@ def upload_file():
     else:
         print(f"Uploads Directory does not exist")
 
+    
     # Assuming file save code is here
     # file.save(filepath)
     if file:
-        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        filepath = os.path.join(uploads_dir, file.filename) # caps 
         file.save(filepath)
+        list_files(uploads_dir)
+
+        connection, channel = get_rabbitmq_connection()
+        channel.queue_declare(queue="image_resize")
+        if connection is None or channel is None:
+            return jsonify({"error": "Failed to connect to RabbitMQ"}), 500
         
         # Publish message to RabbitMQ
         channel.basic_publish(exchange="", routing_key="image_resize", body=filepath)
+        connection.close()  # Close after use
         return jsonify({"message": "File uploaded and processing started!"})
 
 if __name__ == "__main__":
